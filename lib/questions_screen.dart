af50d5fde7e9a2f8a2b2cdc09aa1e8ef08392a20
Mamta Bhatt <610mamtabhatt@gmail.com>
2024-08-09 19:43:15 +0545

    UI of quiz screen is added

---
+++ lib/questions_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('The question...',style: TextStyle(color: Colors.white, fontSize: 24),),
+          SizedBox(height: 30,),
+AnswerButton(answerText: 'Answer 1',
+    onTap: (){
+
+}),
+          AnswerButton(answerText: 'Answer 2',
+              onTap: (){
+
+              }),AnswerButton(answerText: 'Answer 3',
+              onTap: (){
+
+              }),
+
+        ],
+      ),
+    );
+  }
+}
